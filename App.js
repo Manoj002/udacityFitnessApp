bc9c95d1a566b83fa96253875f7291e4665258f6
Manoj002 <ymanoj821@gmail.com>
2018-06-06 17:43:23 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { View, Platform, StatusBar } from 'react-native';
 import { Constants } from 'expo';
-import {  Provider } from 'react-redux';
+import { Provider } from 'react-redux';
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
 import store from './store';
 
 class App extends React.Component {
@@ -10,20 +11,30 @@ class App extends React.Component {
 
   render() {
 
+    const Tabs = createBottomTabNavigator({
+      
+    })
+
+    const MainNavigation = createBottomTabNavigator({
+      home: { Tabs },
+      entryDetail: EntryDetail
+    }, {
+      navigationOptions: {
+        headerTintColor: white,
+        headerStyle: {
+          backgroundColor: purple,
+        }
+      }
+    })
+
     return(
 
       <Provider store={store}>
-
         <View style={{flex: 1}}>
-
           <MainNavigation />
-
         </View>
-
       </Provider>
 
     )
-
   }
-
 }
